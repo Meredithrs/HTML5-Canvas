465681b6218fa74c1f1a2a49ef64ae9fdd843a54
Meredithrs <meredith.xpw@gmail.com>
2014-04-30 15:55:34 -0400

    Create source.js

---
+++ interface-demo-incomplete/source.js
@@ -0,0 +1,8 @@
+/*
+  This file won't actually do anything and will almost certainly be removed by the end of the project.
+  It does allow me to test some drawings, though, so I'm keeping it for now.
+*/
+
+game.drawTile(0, 0, "black", 25);
+game.drawTile(1, 0, "blue", 25);
+game.drawTile(2, 0, "black", 25);
