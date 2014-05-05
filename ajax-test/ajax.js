bef1710d3be5359d179961f54d12e290d9e0b5da
Meredithrs <meredith.xpw@gmail.com>
2014-05-05 18:45:03 -0400

    Update ajax.js

--- ajax-test/ajax.js
+++ ajax-test/ajax.js
@@ -1,12 +1,18 @@
 function Ajax(){
+	this.result	=	"";
+	
 	this.getFile	=	function(url){
 		var result	=	new XMLHttpRequest();
 		result.onreadystatechange	=	function(){
 			if (result.readyState==4 && result.status==200) {
-				return result.responseText;
+				this.result	=	result.responseText;
 			}
 		}
 		result.open("GET", url, true);
 		result.send();
+	}
+	
+	this.getResult	=	function(){
+		return this.result;
 	}
 }
